34a19d527ca395b790bccd8f26ecac96e2742d84
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2020-05-13 21:53:29 +0430

    +adapter using baseAdapter to hold greetings list

---
+++ app/src/main/java/ir/mrahimy/family/ui/splash/greeting/GreetingAdapter.kt
@@ -0,0 +1,28 @@
+package ir.mrahimy.family.ui.splash.greeting
+
+import androidx.recyclerview.widget.DiffUtil
+import ir.mrahimy.family.R
+import ir.mrahimy.family.base.BaseAdapter
+import ir.mrahimy.family.data.pojo.Greeting
+
+/**
+ * Shows a list of greetings by [Greeting][ir.mrahimy.family.data.pojo.Greeting] data class
+ */
+class GreetingAdapter : BaseAdapter<Greeting>(DIFF_CALLBACK) {
+
+    companion object {
+        private val DIFF_CALLBACK = object : DiffUtil.ItemCallback<Greeting>() {
+            override fun areContentsTheSame(oldItem: Greeting, newItem: Greeting): Boolean {
+                return oldItem == newItem
+            }
+
+            override fun areItemsTheSame(oldItem: Greeting, newItem: Greeting): Boolean {
+                return oldItem.title == newItem.title
+            }
+        }
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        return R.layout.item_greeting
+    }
+}
