faf7d7f4454f3b8e41861ea50d5e0d1072439b73
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2020-05-14 00:44:02 +0430

    making person a room entity

--- app/src/main/java/ir/mrahimy/family/data/pojo/Person.kt
+++ app/src/main/java/ir/mrahimy/family/data/pojo/Person.kt
@@ -1,17 +1,32 @@
 package ir.mrahimy.family.data.pojo
 
+import android.os.Parcelable
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.Ignore
 import com.google.gson.annotations.SerializedName
 import ir.mrahimy.family.network.Exclude
+import kotlinx.android.parcel.IgnoredOnParcel
+import kotlinx.android.parcel.Parcelize
 
+@Entity(
+    tableName = "people",
+    primaryKeys = ["firstName", "lastName"]
+)
+@Parcelize
 data class Person(
     @SerializedName("firstName")
+    @ColumnInfo(name = "firstName")
     val firstName: String,
 
     @SerializedName("lastName")
+    @ColumnInfo(name = "lastName")
     val lastName: String
-) {
+) : Parcelable {
 
     @Exclude
+    @Ignore
+    @IgnoredOnParcel
     var fullName = ""
 }
 
