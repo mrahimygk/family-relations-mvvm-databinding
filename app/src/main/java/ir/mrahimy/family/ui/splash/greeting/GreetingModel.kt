5cb860b61e039e1dbc1862eaf402312b900cba0f
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2020-05-13 22:47:06 +0430

    +id to greeting item

--- app/src/main/java/ir/mrahimy/family/ui/splash/greeting/GreetingModel.kt
+++ app/src/main/java/ir/mrahimy/family/ui/splash/greeting/GreetingModel.kt
@@ -2,21 +2,22 @@ package ir.mrahimy.family.ui.splash.greeting
 
 import ir.mrahimy.family.base.BaseModel
 import ir.mrahimy.family.data.pojo.Greeting
+import kotlin.random.Random
 
 class GreetingModel : BaseModel() {
 
     private val greetingList = listOf(
-        Greeting("Bem-vindo"),
-        Greeting("Benvenuti"),
-        Greeting("Willkommen"),
-        Greeting("Salvete"),
-        Greeting("Welcome"),
-        Greeting("Bienvenidos"),
-        Greeting("Welkom"),
-        Greeting("Mabuhay")
+        Greeting(1, "Bem-vindo"),
+        Greeting(2, "Benvenuti"),
+        Greeting(3, "Willkommen"),
+        Greeting(4, "Salvete"),
+        Greeting(5, "Welcome"),
+        Greeting(6, "Bienvenidos"),
+        Greeting(7, "Welkom"),
+        Greeting(8, "Mabuhay")
     )
 
     fun randomGreeting(): Greeting {
-        return greetingList.random()
+        return greetingList.random().copy(id = Random.nextInt(Int.MAX_VALUE))
     }
 }
