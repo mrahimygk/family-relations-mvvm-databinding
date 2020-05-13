e9304f68c4adadee3d2cab2c791da07f7275986a
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2020-05-13 23:39:54 +0430

    adding di for family activity

--- app/src/main/java/ir/mrahimy/family/modules/ModelModule.kt
+++ app/src/main/java/ir/mrahimy/family/modules/ModelModule.kt
@@ -1,5 +1,8 @@
 package ir.mrahimy.family.modules
 
+import ir.mrahimy.family.ui.family.FamilyModel
+import ir.mrahimy.family.ui.family.list.PeopleListModel
+import ir.mrahimy.family.ui.family.relations.FamilyRelationsModel
 import ir.mrahimy.family.ui.splash.SplashModel
 import ir.mrahimy.family.ui.splash.greeting.GreetingModel
 import org.koin.dsl.module
@@ -7,4 +10,7 @@ import org.koin.dsl.module
 val modelModule = module {
     factory { SplashModel() }
     factory { GreetingModel() }
+    factory { PeopleListModel() }
+    factory { FamilyModel() }
+    factory { FamilyRelationsModel() }
 }
