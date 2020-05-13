c6a3b08a69e11f74402e6943bede8414146b3b6e
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2020-05-13 22:32:49 +0430

    adding greeting list to show in recycler views

--- app/src/main/java/ir/mrahimy/family/ui/splash/greeting/GreetingViewModel.kt
+++ app/src/main/java/ir/mrahimy/family/ui/splash/greeting/GreetingViewModel.kt
@@ -6,6 +6,7 @@ import androidx.lifecycle.viewModelScope
 import ir.mrahimy.family.R
 import ir.mrahimy.family.base.BaseViewModel
 import ir.mrahimy.family.data.pojo.Greeting
+import ir.mrahimy.family.util.Event
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
@@ -19,13 +20,24 @@ class GreetingViewModel(model: GreetingModel) : BaseViewModel(model) {
     val splashText: LiveData<Int>
         get() = _splashText
 
+    private val _greetingList = MutableLiveData<List<Greeting>>()
+    val greetingList: LiveData<List<Greeting>>
+        get() = _greetingList
+
     init {
         viewModelScope.launch {
             var shouldFinish = false;
             while (!shouldFinish) {
                 delay(666)
-                _greeting.postValue(model.randomGreeting())
+                val currentList = _greetingList.value?.toMutableList() ?: mutableListOf()
+                _greetingList.postValue(currentList.apply {
+                    add(model.randomGreeting())
+                })
             }
         }
+    }
+
+    fun onGreetingClick(greeting: Greeting) {
+        _snackMessageString.postValue(Event(greeting.title))
     }
 }
