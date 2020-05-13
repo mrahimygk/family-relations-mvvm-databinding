565d360f50f9640679443d5588aa56b3ef5c8539
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2020-05-13 23:37:39 +0430

    +empty vmodel to show family relations

---
+++ app/src/main/java/ir/mrahimy/family/ui/family/relations/FamilyRelationsViewModel.kt
@@ -0,0 +1,6 @@
+package ir.mrahimy.family.ui.family.relations
+
+import ir.mrahimy.family.base.BaseViewModel
+
+class FamilyRelationsViewModel(model: FamilyRelationsModel) : BaseViewModel(model) {
+}
