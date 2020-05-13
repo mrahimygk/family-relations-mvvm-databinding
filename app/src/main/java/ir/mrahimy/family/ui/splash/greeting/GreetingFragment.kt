bc3f371da2b2727b92533afc829ea2fc2cffced0
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2020-05-13 22:48:11 +0430

    using greeting adapter to show list from vmodel

--- app/src/main/java/ir/mrahimy/family/ui/splash/greeting/GreetingFragment.kt
+++ app/src/main/java/ir/mrahimy/family/ui/splash/greeting/GreetingFragment.kt
@@ -4,6 +4,8 @@ import ir.mrahimy.family.R
 import ir.mrahimy.family.base.BaseFragment
 import ir.mrahimy.family.databinding.FragmentGreetingBinding
 import ir.mrahimy.family.ui.splash.SplashViewModel
+import kotlinx.android.synthetic.main.fragment_greeting.*
+import org.koin.android.ext.android.inject
 import org.koin.androidx.viewmodel.ext.android.sharedViewModel
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
@@ -12,8 +14,13 @@ class GreetingFragment : BaseFragment<GreetingViewModel, FragmentGreetingBinding
     override val layoutRes: Int = R.layout.fragment_greeting
     override val sharedViewModel: SplashViewModel by sharedViewModel()
 
+    private val greetingAdapter: GreetingAdapter by inject()
+
     override fun configEvents() {
-//        TODO("Not yet implemented")
+        greetings_recyclerview?.adapter = greetingAdapter
+        greetingAdapter.onItemClicked = { greeting, _ ->
+            viewModel.onGreetingClick(greeting)
+        }
     }
 
     override fun bindObservables() {
