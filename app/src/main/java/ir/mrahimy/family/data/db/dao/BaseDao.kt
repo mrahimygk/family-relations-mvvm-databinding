aa97529b4ffd36d91dea5a2963ba26d3b5449395
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2020-05-14 00:53:01 +0430

    +base dao for general CRUD

---
+++ app/src/main/java/ir/mrahimy/family/data/db/dao/BaseDao.kt
@@ -0,0 +1,19 @@
+package ir.mrahimy.family.data.db.dao
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+
+@Dao
+interface BaseDao<T> {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insert(item: T)
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insert(list: List<T>)
+
+    @Delete
+    suspend fun delete(item: T)
+}
