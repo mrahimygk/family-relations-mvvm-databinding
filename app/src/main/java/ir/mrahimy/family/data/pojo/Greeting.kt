ccbb8e5b132f9cb084c944ae94dec4fde4b704ea
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2020-05-13 21:44:04 +0430

    +simple parcelized data class to be used in navigation argument

---
+++ app/src/main/java/ir/mrahimy/family/data/pojo/Greeting.kt
@@ -0,0 +1,9 @@
+package ir.mrahimy.family.data.pojo
+
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+data class Greeting(val title: String) : Parcelable {
+
+}
