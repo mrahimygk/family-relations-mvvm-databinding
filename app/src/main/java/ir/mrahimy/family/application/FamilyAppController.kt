5716043430b9a837504c589054703b198957b2c1
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2020-05-13 21:45:09 +0430

    adding application controller to start koin an plant timber logging.

---
+++ app/src/main/java/ir/mrahimy/family/application/FamilyAppController.kt
@@ -0,0 +1,43 @@
+package ir.mrahimy.family.application
+
+import android.app.Activity
+import android.app.Application
+import ir.mrahimy.family.BuildConfig
+import ir.mrahimy.family.modules.adapterModule
+import ir.mrahimy.family.modules.modelModule
+import ir.mrahimy.family.modules.viewModelModule
+import ir.mrahimy.family.network.ConnectionLiveData
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+import org.koin.core.logger.Level
+import timber.log.Timber
+
+/**
+ * Custom application class
+ */
+class FamilyAppController : Application() {
+
+    var currentActivity: Activity? = null
+
+    override fun onCreate() {
+        super.onCreate()
+
+        /**
+         * Planting Timber logger in debug mode
+         */
+        if (BuildConfig.DEBUG) Timber.plant(Timber.DebugTree())
+
+        //Network Connectivity
+        ConnectionLiveData.init(this)
+
+        /**
+         * all injection modules should be added here as a koin module.
+         */
+        startKoin {
+            androidContext(this@FamilyAppController)
+            androidLogger(Level.DEBUG)
+            modules(viewModelModule, modelModule, adapterModule)
+        }
+    }
+}
