e965c657fc6dcd1a5df3cce549b653ac6ba15556
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2020-05-13 23:43:02 +0430

    +simple navigation to test command pattern

--- app/src/main/java/ir/mrahimy/family/ui/family/list/PeopleListViewModel.kt
+++ app/src/main/java/ir/mrahimy/family/ui/family/list/PeopleListViewModel.kt
@@ -1,6 +1,16 @@
 package ir.mrahimy.family.ui.family.list
 
+import androidx.lifecycle.viewModelScope
 import ir.mrahimy.family.base.BaseViewModel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class PeopleListViewModel(model: PeopleListModel) : BaseViewModel(model) {
+
+    init {
+        viewModelScope.launch {
+            delay(500)
+            navigateTo(PeopleListFragmentDirections.actionPeopleListToInferredData())
+        }
+    }
 }
