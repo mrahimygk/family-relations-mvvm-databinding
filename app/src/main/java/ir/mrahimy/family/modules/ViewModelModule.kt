b6823d19a689d072f93d8e5e22cdff9d375775ec
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2020-05-13 23:45:04 +0430

    +family package to di

--- app/src/main/java/ir/mrahimy/family/modules/ViewModelModule.kt
+++ app/src/main/java/ir/mrahimy/family/modules/ViewModelModule.kt
@@ -1,5 +1,8 @@
 package ir.mrahimy.family.modules
 
+import ir.mrahimy.family.ui.family.FamilyViewModel
+import ir.mrahimy.family.ui.family.list.PeopleListViewModel
+import ir.mrahimy.family.ui.family.relations.FamilyRelationsViewModel
 import ir.mrahimy.family.ui.splash.SplashViewModel
 import ir.mrahimy.family.ui.splash.greeting.GreetingViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
@@ -8,4 +11,7 @@ import org.koin.dsl.module
 val viewModelModule = module {
     viewModel { SplashViewModel(get()) }
     viewModel { GreetingViewModel(get()) }
+    viewModel { FamilyViewModel(get()) }
+    viewModel { PeopleListViewModel(get()) }
+    viewModel { FamilyRelationsViewModel(get()) }
 }
