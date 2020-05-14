46d1a09724f936c2e8b5849bf90a87c001afc0f1
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2020-05-14 10:25:57 +0430

    providing people repo to people list model

--- app/src/main/java/ir/mrahimy/family/ui/family/list/PeopleListModel.kt
+++ app/src/main/java/ir/mrahimy/family/ui/family/list/PeopleListModel.kt
@@ -1,6 +1,11 @@
 package ir.mrahimy.family.ui.family.list
 
 import ir.mrahimy.family.base.BaseModel
+import ir.mrahimy.family.repository.PeopleRepository
 
-class PeopleListModel : BaseModel() {
+class PeopleListModel(private val peopleRepository: PeopleRepository) : BaseModel() {
+
+    fun getAll() = peopleRepository.getAllFromLocalDb()
+
+    suspend fun syncPeople() = peopleRepository.sync()
 }
