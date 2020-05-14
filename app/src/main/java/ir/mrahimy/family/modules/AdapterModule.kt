f63c8526046301f4afd0f549389c9c93fa4bdcf0
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2020-05-14 10:23:27 +0430

    adding people adapter to koin service

--- app/src/main/java/ir/mrahimy/family/modules/AdapterModule.kt
+++ app/src/main/java/ir/mrahimy/family/modules/AdapterModule.kt
@@ -1,8 +1,10 @@
 package ir.mrahimy.family.modules
 
+import ir.mrahimy.family.ui.family.list.PeopleAdapter
 import ir.mrahimy.family.ui.splash.greeting.GreetingAdapter
 import org.koin.dsl.module
 
 val adapterModule = module {
     factory { GreetingAdapter() }
+    factory { PeopleAdapter() }
 }
