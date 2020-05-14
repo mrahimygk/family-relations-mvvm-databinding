1960c4e199e4c73afe99512f112938a945480cdd
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2020-05-14 14:37:32 +0430

    adding adapter to di

--- app/src/main/java/ir/mrahimy/family/modules/AdapterModule.kt
+++ app/src/main/java/ir/mrahimy/family/modules/AdapterModule.kt
@@ -1,10 +1,12 @@
 package ir.mrahimy.family.modules
 
 import ir.mrahimy.family.ui.family.list.PeopleAdapter
+import ir.mrahimy.family.ui.family.relations.PeopleRelationsAdapter
 import ir.mrahimy.family.ui.splash.greeting.GreetingAdapter
 import org.koin.dsl.module
 
 val adapterModule = module {
     factory { GreetingAdapter() }
     factory { PeopleAdapter() }
+    factory { PeopleRelationsAdapter() }
 }
