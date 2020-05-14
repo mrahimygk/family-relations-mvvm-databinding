cf9346c8f2fe29e55c07f31ac90260bf4ddbc2d7
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2020-05-14 14:35:40 +0430

    +shared relations adapter

--- app/src/main/java/ir/mrahimy/family/ui/family/relations/FamilyRelationsViewModel.kt
+++ app/src/main/java/ir/mrahimy/family/ui/family/relations/FamilyRelationsViewModel.kt
@@ -1,6 +1,16 @@
 package ir.mrahimy.family.ui.family.relations
 
+import androidx.lifecycle.MutableLiveData
 import ir.mrahimy.family.base.BaseViewModel
+import ir.mrahimy.family.data.pojo.PersonRelations
+import ir.mrahimy.family.util.Shared
 
 class FamilyRelationsViewModel(model: FamilyRelationsModel) : BaseViewModel(model) {
+    /**
+     * getting inferred list by db query. This is in the exact Karaf's code challenge requirements
+     */
+    val inferredList = MutableLiveData<List<String>>()
+
+    @Shared("inferredPeopleRelations")
+    val inferredPeopleRelations = MutableLiveData<List<PersonRelations>>()
 }
