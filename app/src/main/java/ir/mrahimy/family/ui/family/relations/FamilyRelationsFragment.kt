225e6a7810c7888050656872fcbc5135756c8008
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2020-05-14 14:35:27 +0430

    +relations adapter

--- app/src/main/java/ir/mrahimy/family/ui/family/relations/FamilyRelationsFragment.kt
+++ app/src/main/java/ir/mrahimy/family/ui/family/relations/FamilyRelationsFragment.kt
@@ -5,15 +5,19 @@ import org.koin.androidx.viewmodel.ext.android.viewModel
 import ir.mrahimy.family.R
 import ir.mrahimy.family.databinding.FragmentFamilyRelationsBinding
 import ir.mrahimy.family.ui.family.FamilyViewModel
+import kotlinx.android.synthetic.main.fragment_family_relations.*
+import org.koin.android.ext.android.inject
 import org.koin.androidx.viewmodel.ext.android.sharedViewModel
 
-class FamilyRelationsFragment : BaseFragment<FamilyRelationsViewModel, FragmentFamilyRelationsBinding>() {
+class FamilyRelationsFragment :
+    BaseFragment<FamilyRelationsViewModel, FragmentFamilyRelationsBinding>() {
     override val viewModel: FamilyRelationsViewModel by viewModel()
     override val layoutRes: Int = R.layout.fragment_family_relations
     override val sharedViewModel: FamilyViewModel by sharedViewModel()
+    private val peopleRelationsAdapter: PeopleRelationsAdapter by inject()
 
     override fun configEvents() {
-//        TODO("Not yet implemented")
+        people_relations_recyclerview?.adapter = peopleRelationsAdapter
     }
 
     override fun bindObservables() {
