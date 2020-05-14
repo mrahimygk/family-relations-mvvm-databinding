e31c08d014c91e66a9051e450dd233d8ff1fdb04
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2020-05-14 10:27:26 +0430

    getting people list from model and mapping to live data for rv.

--- app/src/main/java/ir/mrahimy/family/ui/family/list/PeopleListViewModel.kt
+++ app/src/main/java/ir/mrahimy/family/ui/family/list/PeopleListViewModel.kt
@@ -1,16 +1,22 @@
 package ir.mrahimy.family.ui.family.list
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.map
 import androidx.lifecycle.viewModelScope
 import ir.mrahimy.family.base.BaseViewModel
-import kotlinx.coroutines.delay
+import ir.mrahimy.family.data.pojo.Person
+import ir.mrahimy.family.data.pojo.fillData
 import kotlinx.coroutines.launch
 
 class PeopleListViewModel(model: PeopleListModel) : BaseViewModel(model) {
 
+    val peopleList: LiveData<List<Person>> = model.getAll().map { list ->
+        list.map { it.fillData() }
+    }
+
     init {
         viewModelScope.launch {
-            delay(500)
-            navigateTo(PeopleListFragmentDirections.actionPeopleListToInferredData())
+            model.syncPeople()
         }
     }
 }
