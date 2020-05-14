de8219639ba246878b304000ae0aa4ac5064f72b
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2020-05-14 10:28:41 +0430

    using people adapter in people list fragment

--- app/src/main/java/ir/mrahimy/family/ui/family/list/PeopleListFragment.kt
+++ app/src/main/java/ir/mrahimy/family/ui/family/list/PeopleListFragment.kt
@@ -4,6 +4,8 @@ import ir.mrahimy.family.R
 import ir.mrahimy.family.base.BaseFragment
 import ir.mrahimy.family.databinding.FragmentPeopleListBinding
 import ir.mrahimy.family.ui.family.FamilyViewModel
+import kotlinx.android.synthetic.main.fragment_people_list.*
+import org.koin.android.ext.android.inject
 import org.koin.androidx.viewmodel.ext.android.sharedViewModel
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
@@ -11,9 +13,10 @@ class PeopleListFragment : BaseFragment<PeopleListViewModel, FragmentPeopleListB
     override val viewModel: PeopleListViewModel by viewModel()
     override val layoutRes: Int = R.layout.fragment_people_list
     override val sharedViewModel: FamilyViewModel by sharedViewModel()
+    private val peopleAdapter: PeopleAdapter by inject()
 
     override fun configEvents() {
-//        TODO("Not yet implemented")
+        people_recyclerview?.adapter = peopleAdapter
     }
 
     override fun bindObservables() {
